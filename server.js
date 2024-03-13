e780c563cc43d6c7aba97906ecd7d7c64eeb6799
Dany Entezari <dany.entezari@gmail.com>
2024-03-13 19:04:44 +0400

    Added CORS and URL modification.

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
-const express = require('express');
-const axios = require('axios');
-const cheerio = require('cheerio');
+const express = require("express");
+const axios = require("axios");
+const cheerio = require("cheerio");
+const cors = require("cors"); // Import the cors middleware
 
 const app = express();
 const port = 3050;
 
 app.use(express.json());
+app.use(cors()); // Use the cors middleware
 
-app.post('/metadata', async (req, res) => {
+app.post("/metadata", async (req, res) => {
   const { url } = req.body;
 
   try {
@@ -19,19 +21,23 @@ app.post('/metadata', async (req, res) => {
     const $ = cheerio.load(html);
 
     // Extract title and favicon
-    const title = $('title').text();
-    const favicon = $('link[rel="icon"]').attr('href') || $('link[rel="shortcut icon"]').attr('href');
+    const title = $("title").text();
+    const favicon =
+      $('link[rel="icon"]').attr("href") ||
+      $('link[rel="shortcut icon"]').attr("href");
 
     // Send JSON response with the desired format
     res.json({ title, image: favicon });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'An error occurred while fetching metadata.' });
+    res
+      .status(500)
+      .json({ error: "An error occurred while fetching metadata." });
   }
 });
 
 // GET route for metadata retrieval
-app.get('/metadata', async (req, res) => {
+app.get("/metadata", async (req, res) => {
   const { url } = req.query;
 
   try {
@@ -43,17 +49,24 @@ app.get('/metadata', async (req, res) => {
     const $ = cheerio.load(html);
 
     // Extract title and favicon
-    const title = $('title').text();
-    const favicon = $('link[rel="icon"]').attr('href') || $('link[rel="shortcut icon"]').attr('href');
+    const title = $("title").text();
+    const favicon =
+      $('link[rel="icon"]').attr("href") ||
+      $('link[rel="shortcut icon"]').attr("href");
+
+    const modifiedURL =
+      url[url.length - 1] === "/" ? url.substring(0, url.length - 1) : url;
 
     // Send JSON response with the desired format
-    res.json({ 
-      title, 
-      image: `${url}${favicon}` 
+    res.json({
+      title,
+      image: `${modifiedURL}/${favicon.replace("/", "")}`,
     });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'An error occurred while fetching metadata.' });
+    res
+      .status(500)
+      .json({ error: "An error occurred while fetching metadata." });
   }
 });
 
